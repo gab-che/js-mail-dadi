2b92f562ec6fc180affea06f96a50439a7f32c46
gab-che <gabriellachessa1@gmail.com>
2022-11-03 18:36:07 +0100

    small improvements

--- js/script-mail.js
+++ js/script-mail.js
@@ -3,23 +3,28 @@ const coolButton = document.getElementById("cool_button")
 coolButton.addEventListener("click", function (){
     const emailList = ["ciao@gmail.com", "ciaociao@gmail.com", "ciaociaociao@gmail.com"];
 
-    const userEmail = document.getElementById("user_email").value;
+    const userEmail = document.getElementById("user_email");
+
+    const answer = document.getElementById("answer");
 
     let found;
 
     for (let x = 0; x < emailList.length; x++){
         const coolEmail = emailList[x];
 
-        if (coolEmail === userEmail){
+        if (coolEmail === userEmail.value){
             found = true;
         }
     }
 
     if (found){
-        console.log("Complimenti! Fai parte del club esclusivo.");
+        answer.innerHTML = "Complimenti! Fai parte del club esclusivo.";
     } else{
-        console.log("Mi dispiace, non sei abbastanza cool");
+        answer.innerHTML = "Mi dispiace, non sei abbastanza cool.";
     }
+
+    coolButton.setAttribute("disabled", "");
+    userEmail.setAttribute("disabled", "");
 })
 
 const tryAgain = document.getElementById("try_again");
@@ -27,4 +32,8 @@ const tryAgain = document.getElementById("try_again");
 tryAgain.addEventListener("click", function (){
     const userEmail = document.getElementById("user_email");
     userEmail.value = "";
+    userEmail.removeAttribute("disabled", "");
+    const answer = document.getElementById("answer");
+    answer.innerHTML = "";
+    coolButton.removeAttribute("disabled", "");
 })
